f0d42ae2417ffb4fc393c3f7a1a9cd7e1b000957
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2025-06-15 19:36:32 +0300

    HWVE-0000 working initialized app

---
+++ src/main/java/com/example/spring_app_caching_proxy/SpringAppCachingProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_caching_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCachingProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppCachingProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_app_caching_proxy/SpringAppCachingProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_caching_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppCachingProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
